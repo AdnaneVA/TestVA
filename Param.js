cafc4636b102dde0c0253cd7da65609f01920497
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-03-26 13:21:10 +0200

    Create Param.js

---
+++ Param.js
@@ -0,0 +1,73 @@
+import React from 'react';
+// import './Parameters.css';
+import MobileTearSheet from '../../MobileTearSheet';
+import List, {ListItem, 
+  ListItemSecondaryAction,
+  ListItemText
+} from 'material-ui/List'; 
+import Subheader from 'material-ui/Subheader'; 
+import Divider from 'material-ui/Divider'; 
+import Checkbox from 'material-ui/Checkbox';
+import Toggle from 'material-ui/Toggle'; 
+
+class CheckboxInteractivity extands React.Component{
+  state= { 
+    checked : false,
+  }
+  
+  updateCheck() {
+    this.setState((oldState) => {
+    return {
+      checked : !old.State.checked,
+    }; 
+  });
+}
+
+render () {
+  return(
+  
+ 
+
+  
+
+const Parameters = () => (
+  <div>
+    <MobileTearSheet>
+    <List>
+      primaryText="Parameters"
+     />
+     </List>
+     <Divider />
+     <List>
+      <Subheader>Sensors</Subheader>
+      <ListItem
+        primaryText= "IR Sensor" rightToggle={<Toggle/>}
+        checked={this.state.checked}
+        onCheck={this.updateCheck.bind(this)}
+        />
+     </List>
+     <Divider />
+     <List>
+      <Subheader>Cloud</Subheader>
+      <ListItem PrimaryText="ComCloud" rightToggle={<Toggle/>}
+      checked={this.state.checked}
+      onCheck={this.updateCheck.bind(this)}
+      secondaryText="Allow communication to the Cloud" 
+      />
+      
+      <ListItem primaryText="AWS" leftCheckbox={<Checkbox/>} 
+      checked={this.state.checked}
+      onCheck={this.updateCheck.bind(this)}
+      />
+      <ListItem primaryText="Azure" leftCheckbox={<Checkbox/>} 
+      checked={this.state.checked}
+      onCheck={this.updateCheck.bind(this}
+      />
+     </List>
+    <MobileTearSheet>
+    </div>
+    );
+    }
+); 
+
+export default Parameters;
