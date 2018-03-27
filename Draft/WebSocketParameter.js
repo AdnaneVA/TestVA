9bc98042b3a9f1f8d3702efd6ae446c8fc9932f3
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-03-27 18:04:28 +0200

    Update WebSocketParameter.js

--- Draft/WebSocketParameter.js
+++ Draft/WebSocketParameter.js
@@ -1,9 +1,9 @@
 import openSocket from 'socket.io-client';
 const  socket = openSocket('http://10.3.141.1:8000');
-import Parameters from './Parameters.js';
-
-function sendParameter() {
-  socket.emit('xxx')
+import Parameter from './Parameter.js';
+//xxx ???  
+function sendParameter(object) {
+  socket.emit(object)
 }; 
 
 
