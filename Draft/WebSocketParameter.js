bf8d68cad27af66a5e0fded1cf544167f18b5e96
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-03-29 13:23:24 +0200

    Update WebSocketParameter.js

--- Draft/WebSocketParameter.js
+++ Draft/WebSocketParameter.js
@@ -1,11 +1,22 @@
 import openSocket from 'socket.io-client';
 const  socket = openSocket('http://10.3.141.1:8000');
 import Parameter from './Parameter.js';
-//xxx ???  
-
-function sendParameter(x) {
-  var paramcheck;
-  if (document.getElementById('Parameter').checked){paramcheck=true;}
-  else{paramcheck=false;}
-  socket.emit(paramcheck); 
-}; 
+<Parameter>
+  
+var x=document.getElementById("main"); 
+var y=x.getElementsByTagName("ListItem"); 
+// y[0] -> IR sensor,  y[1]-> ComCloud, y[2] -> AWS, y[3] -> Azure 
+var paramList={IRSensor:false, ComCloud:false, AWS:false, Azure: false};      
+function sendParameter() {
+  paramList.IRSensor = (y[0].checked) ? true:false; 
+  paramList.ComCloud = (y[1].checked) ? true:false;
+  paramList.AWS = (y[2].checked) ? true:false; 
+  paramList.Azure = (y[3].checked) ?  true:false; 
+  socket.emit(paramList); 
+}
+    
+  
+   
+                     
+      
+  
