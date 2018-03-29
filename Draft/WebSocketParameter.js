09df03c27e62697e162ddec3a695482144397e53
AdnaneVA <37668449+AdnaneVA@users.noreply.github.com>
2018-03-29 10:52:25 +0200

    Update WebSocketParameter.js

--- Draft/WebSocketParameter.js
+++ Draft/WebSocketParameter.js
@@ -2,8 +2,10 @@ import openSocket from 'socket.io-client';
 const  socket = openSocket('http://10.3.141.1:8000');
 import Parameter from './Parameter.js';
 //xxx ???  
-function sendParameter(object) {
-  socket.emit(object)
+
+function sendParameter(x) {
+  var paramcheck;
+  if (document.getElementById('Parameter').checked){paramcheck=true;}
+  else{paramcheck=false;}
+  socket.emit(paramcheck); 
 }; 
-
-
